448b138917df42412bfa4b647009e435b356b8e2
Pedro I. R. Luz de Castro <61996394+prldc@users.noreply.github.com>
2020-12-12 14:39:23 -0300

    Add files via upload

---
+++ merge_tweets.R
@@ -0,0 +1,37 @@
+require(tidyverse)
+library(readr)
+
+`%notin%` <- Negate(`%in%`)
+
+Bolsonaro <- read_csv("jairbolsonaro.csv")
+Marina <- read_csv("MarinaSilva.csv")
+Alckmin <- read_csv("geraldoalckmin.csv")
+Haddad <- read_csv("Haddad_Fernando.csv")
+Lula <- read_csv("LulaOficial.csv")
+Boulos <- read_csv("GuilhermeBoulos.csv")
+Ciro <- read_csv("cirogomes.csv")
+
+
+combinado <- bind_rows(
+  Bolsonaro %>%
+    mutate(person = "Bolsonaro"),
+  Marina %>%
+    mutate(person = "Marina"),
+  Alckmin %>%
+    mutate(person = "Alckmin"),
+  Boulos %>%
+    mutate(person = "Boulos"),
+  Haddad %>%
+    mutate(person = "Haddad"),
+  Lula %>%
+    mutate(person = "Lula"),
+  Ciro %>%
+    mutate(person = "Ciro"),
+)
+
+combinado <- combinado %>% 
+  mutate(periodo_eleitoral = case_when(
+    (combinado$date < as.Date('2018-10-29'))&(combinado$date > as.Date('2018-07-07')) ~ T,
+                                                                         T ~ F))
+
+write.csv(combinado, 'tweets_presidenciaveis.csv', row.names = F)
